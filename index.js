e41e7f0630b5246c76f2eebb8c239f6c9900e1c2
David <avilagarciajesusdavid41@gmail.com>
2024-07-18 22:38:49 -0600

    Insercion back

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./userRoutes');
+const productRoutes = require('./productRoutes');
+const orderRoutes = require('./orderRoutes');
+const { poolPromise } = require('./db');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'https://web-store-two-zeta.vercel.app/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send("Hola, este es tu back");
+});
+
+app.get('/get/test/db', async (req, res) => {
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query('SELECT COUNT(*) as count FROM Usuarios');
+      res.json({ count: result.recordset[0].count });
+    } catch (err) {
+      console.error('SQL error', err);
+      res.status(500).json({ error: err.message });
+    }
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/', orderRoutes);
+  
+app.use((req, res, next) => {
+  res.status(404).send("Página no encontrada");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
